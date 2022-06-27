a774ac52897d043bacdca1a55921fc5fe0facbbc
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-26 22:57:08 -0300

    setting up

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return <div>
+    <form>
+      <input type="text" />
+      <button>Agregar</button>
+    </form>
+    <ul>
+      <li>Items</li>
+      <li>Items</li>
+    </ul>
+  </div>
+}
+
+export default App
